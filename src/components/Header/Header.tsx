196dae1b96e04662c831f18c444bcce8aa48fa9d
Hitesh Thakur <hiteshthakur695#gmail.com>
2025-02-22 18:57:29 +0530

    df

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React from 'react';
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import styles from './style.module.css';
 import { usePathname } from 'next/navigation';
