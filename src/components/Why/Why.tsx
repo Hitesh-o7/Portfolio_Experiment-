1a5bcf8a04bd54663aa6772b7b0f1a005f445f7e
Hitesh Thakur <hiteshthakur695#gmail.com>
2025-05-27 22:07:20 +0530

    ui change

--- src/components/Why/Why.tsx
+++ src/components/Why/Why.tsx
@@ -1,36 +1,28 @@
 import React from "react";
 import "./Why.css";
- 
- 
 
 const Why: React.FC = () => {
-  
   return (
     <div className="why-container">
       <div className="left-section">
         <div className="sticky-container">
-          <div className="sticky-text ">Why Work With Me</div>
+          <div className="sticky-text">My Skillset</div>
         </div>
       </div>
 
       <div className="right-section">
-      
         <div className="grid grid-cols-2 mt-200">
-          <div className="border border-white/30    text-2xl h-[350px] p-4 flex items-end  ">
-            UI DESIGN
+          <div className="border border-[#191919] text-2xl h-[350px] p-4 flex items-end">
+            <p>UI DESIGN</p>
           </div>
-          <div className="p-4 h-[350px] flex items-end  "> </div>
-          <div className="p-4 h-[350px] flex items-end  "> </div>
-          <div className="border border-white/30 h-[350px] text-2xl border-white/50 p-4 flex items-end  ">
-            Brand Design
-          </div>
-          <div className="border border-white/30 h-[350px] text-2xl border-white/50 p-4 flex items-end  ">
-            UX Design
-          </div>
-          <div className="p-4 h-[350px] flex items-end justify-center"> </div>
-          <div className="p-4 h-[350px] flex items-end justify-center"> </div>
-          <div className="border border-white/30 h-[350px]  text-2xl border-white/50 p-4 flex items-end  ">
-            No-code development
+          <div className="p-4 h-[350px] flex items-end"></div>
+          <div className="p-4 h-[350px] flex items-end"></div>
+          <div className="border border-[#191919] h-[350px] text-2xl border-white/50 p-4 flex items-end"><p>BRAND DESIGN</p></div>
+          <div className="border border-[#191919] h-[350px] text-2xl border-white/50 p-4 flex items-end"><p>UX DESIGN</p></div>
+          <div className="p-4 h-[350px] flex items-end justify-center"></div>
+          <div className="p-4 h-[350px] flex items-end justify-center"></div>
+          <div className="border border-[#191919] h-[350px] text-2xl border-white/50 p-4 flex items-end">
+            <p>NO-CODE DEVELOPMENT</p>
           </div>
         </div>
       </div>
